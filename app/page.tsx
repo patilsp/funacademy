d6df12797a93e65bc69f3a78b67a40d0efd3decf
santoshpatil96 <santosh.patil@fugoservices.com>
2024-08-29 14:36:33 +0530

    ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,35 +12,34 @@ import { SparklesPreview } from '@/components/sparkles-preview';
 
 export default function Home() {
   return (
-      <div className="relative z-20 h-screen bg-[#ffc854]">
+      <div className="relative z-20 h-screen bg-white dark:bg-black dark:text-white">
       
       <div className="absolute"></div>
       <div className="absolute inset-0 hidden md:block">
         <PulseBeams />
       </div>
 
-      <div className="relative inset-0 flex flex-col items-center justify-center bg-[#ffc854] text-center md:hidden">
-      <h1 className="mt-5 text-center text-3xl font-bold text-white md:text-7xl lg:text-9xl">
+      <div className="relative inset-0 flex flex-col items-center justify-center  text-center md:hidden">
+      <h1 className="mt-20 text-center text-4xl font-bold md:text-7xl lg:text-9xl">
         Fun Academy
       </h1>
        
-        <h1 className=" text-bold mx-auto my-10 max-w-md text-4xl">
+        <h1 className="text-bold mx-auto my-10 max-w-md text-4xl">
           Become an <br />expert by learning <br /><span className="text-bold text-[#ff5722]">once </span> a day  
         </h1>
-      
-        <Link href="/" className="my-10 pt-6">
-        
-          <Image
-            src="/images/bg-register.png"
+        <div className="my-10">
+        <Image
+            src="/images/welcome2.jpg"
             className="size-full object-contain "
             width={500}
             height={700}
             alt="logo image"            
           />
-        </Link>
 
+
+          </div>
         <motion.div
-          className="relative mb-4 mt-24 flex items-center justify-center"
+          className="relative mb-4 mt-10 flex items-center justify-center"
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, ease: 'easeOut' }}
@@ -56,7 +55,10 @@ export default function Home() {
           </button>
 
           </div>
+
+          
         </motion.div>
+        
       </div>
     </div>
   );
