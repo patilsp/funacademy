6dc934ffbb512b0309ad1e807984819c411e038e
santoshpatil96 <santoshpatil4510@gmail.com>
2026-09-17 12:53:53 +0530

    old code removed

--- models/response.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const responseSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  questions: { type: Array, required: true },
-  selectedOptions: { type: Array, required: true },
-});
-
-export default mongoose.models.Response || mongoose.model('Response', responseSchema);
